89bdd529f83cadb082700322b171cacb897dd5ff
Gabjea <gabjea856@gmail.com>
2020-07-24 15:47:16 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/inkless/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.inkless
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/FirstScreen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import 'SecondScreen.dart';
+
+class FirstScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext ctxt) {
+    return new Scaffold(
+        appBar: new AppBar(
+          title: new Text("Multi Page Application"),
+        ),
+        body: new Checkbox(
+            value: false,
+            onChanged: (bool newValue) {
+              Navigator.push(
+                ctxt,
+                new MaterialPageRoute(builder: (ctxt) => new SecondScreen()),
+              );
+            }));
+  }
+}
